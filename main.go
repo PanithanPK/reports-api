7fd6900c99509c0b293ef9f7a7210d084e21c7e2
PanithanPK <lovee12345e@gmail.com>
2025-07-19 11:54:43 +0700

    fix: fix bug

--- main.go
+++ main.go
@@ -138,21 +138,21 @@ func main() {
 	// r.Use(middleware.BasicSecurityHeadersMiddleware)
 
 	// Disable CORS by allowing all origins
-	r.Use(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			
-			if r.Method == "OPTIONS" {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-			
-			next.ServeHTTP(w, r)
-		})
-	})
-	logger.Info.Println("🌐 CORS disabled - allowing all origins")
+	// r.Use(func(next http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		w.Header().Set("Access-Control-Allow-Origin", "*")
+	// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	// 		if r.Method == "OPTIONS" {
+	// 			w.WriteHeader(http.StatusOK)
+	// 			return
+	// 		}
+
+	// 		next.ServeHTTP(w, r)
+	// 	})
+	// })
+	// logger.Info.Println("🌐 CORS disabled - allowing all origins")
 
 	// Serve static files
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./fontend"))))
